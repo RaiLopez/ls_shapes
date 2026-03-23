9fd449c5d5c3788da4cc65d0ee9dc79a192b71aa
Rai <rai.lopez@outlook.com>
2026-03-23 15:15:26 +0100

    Auto-build: 2026-03-23 15:15

--- Menu/- Lost Scripts/ls_shapes.lua
+++ Menu/- Lost Scripts/ls_shapes.lua
@@ -9,7 +9,7 @@ ScriptVersion = "0.4.2"
 ScriptStage = "BETA"
 ScriptTarget = "14.3+ Pro"
 ScriptDep = {"Utility\ls_utilities.lua", "Modules/ls.lua"}
-ScriptDesc = "A persistent shape palette plus helpers for better Moho&reg; vectors management." -- ⚠ AVOID UNICODE! (Use HTML entities)
+ScriptDesc = "Persistent shape palette plus helpers for better Moho&reg; vector/s management." -- ⚠ AVOID UNICODE! (Use HTML entities)
 
 -- **************************************************
 -- General information about this script
