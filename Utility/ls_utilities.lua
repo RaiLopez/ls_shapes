c791d7f948970dffe64db2b3c628b0ad4c2763f7
Rai <rai.lopez@outlook.com>
2026-04-09 22:36:25 +0200

    Update: BUILME.sh, README.md (@0d4e282)

--- Utility/ls_utilities.lua
+++ Utility/ls_utilities.lua
@@ -1,4 +1,17 @@
-ls_utilities
+-- **************************************************
+-- Provide Moho with the name+ of this script object
+-- **************************************************
 
--- 20260318-0255
--- 20260323-1734
+ScriptName = "LS_Utils"
+ScriptBirth = "20220101-0101"
+ScriptBuild = "20260409-2235"
+ScriptVersion = "0.4.2"
+ScriptStage = "BETA"
+ScriptTarget = "14.3+ Pro"
+ScriptDesc = "Essential shared resources, utilities, and core modules required for the Lost Scripts&trade; project to work with MOHO&reg; Pro Animation Software." -- ⚠ AVOID UNICODE! (Use HTML entities)
+
+-- **************************************************
+-- General information about this script
+-- **************************************************
+
+LS_Utils = {}
