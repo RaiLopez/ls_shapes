d6189bf7e531305e7b9e90c66c824d3b41eb1921
Rai <rai.lopez@outlook.com>
2026-04-12 01:31:54 +0200

    Update: BUILDME.sh (@bc8fa8a)

--- Utility/ls_utilities.lua
+++ Utility/ls_utilities.lua
@@ -5,9 +5,9 @@
 ScriptName = "LS_Utilities"
 ScriptBirth = "20220101-0101"
 ScriptBuild = "20260409-2235"
-ScriptVersion = "0.0.1"
+ScriptVersion = "0.0.0"
 ScriptStage = "BETA"
-ScriptTarget = "14 Pro"
+ScriptTarget = "14 Pro (CURRENTLY UNAVAILABLE)"
 ScriptDesc = "Essential shared resources and core modules required for the <a href='https://lost-scripts.github.io/' title='Go to Lost Scripts&trade; website...'>Lost Scripts</a>&trade; project to work with <a href='https://moho.lostmarble.com/' title='Go to Moho&reg; homepage...'>MOHO</a><sup>&nbsp;Pro</sup> Animation Software." -- ⚠ AVOID UNICODE! (Use "HTML" entities)
 
 -- **************************************************
